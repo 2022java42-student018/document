c37e2889601cf24f606f5b4dcaa128aa5fea172e
WIN-ITQE3H50UG3\student <2022java42-student025@la-study.com>
2022-05-31 14:20:28 +0900

    森田

---
+++ Bteam.sql(森田）1.sql
@@ -0,0 +1,5 @@
+insert into item(isbn , dName , cCode , aName, pName , pDate)values(1451213454,'­—˜_',1,'¡ì','W‰pĞ','2004-09-01');
+INSERT INTO customer(cName,cAddress,cTell,cMail,cBday)values('²“¡','“Œ‹“s','09022222222','aaa@aaa','1998-01-01');
+
+UPDATE item SET rescid = NULL WHERE did = 1;
+
